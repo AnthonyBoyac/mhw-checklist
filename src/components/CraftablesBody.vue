40d7883ee7efaab4119e030c54c8b9f8dcc883f1
AnthonyBoyac <anthony.f.boyac@gmail.com>
2021-05-27 16:10:06 -0700

    Added 'table description' prop

--- src/components/CraftablesBody.vue
+++ src/components/CraftablesBody.vue
@@ -5,7 +5,10 @@
     :clearedItems="clearedItems"
     :setDecos="setDecos"
     :progressCounter="progressCounter"
-    :maxProgressCount="maxProgressCount" />
+    :maxProgressCount="maxProgressCount"
+    tableDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    progressName="Decorated gear"
+    charmProgressName="Crafted gear"/>
     <table id="gear-table">
       <thead>
         <tr>
